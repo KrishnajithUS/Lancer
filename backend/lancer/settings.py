2ee8b9981a198055d7929335d3e266d49f77c7f6
Krishnajith <97077068+KrishnajithUS@users.noreply.github.com>
2023-01-06 21:28:18 +0530

    removed

--- backend/lancer/settings.py
+++ backend/lancer/settings.py
@@ -191,12 +191,7 @@ env = environ.Env()
 environ.Env.read_env()
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'krishnajithuslinuxbackup@gmail.com'
-EMAIL_HOST_PASSWORD ='xclxywhpxpnsujxu'
+
 
 
 
