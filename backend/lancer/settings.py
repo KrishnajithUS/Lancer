66a95ffffe5a6757121beb492c8f47541430bcdd
krishnajith <krishnajithus673@gmail.com>
2023-01-06 21:33:38 +0530

    account secured

--- backend/lancer/settings.py
+++ backend/lancer/settings.py
@@ -195,8 +195,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'krishnajithuslinuxbackup@gmail.com'
-EMAIL_HOST_PASSWORD ='xclxywhpxpnsujxu'
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD =env('EMAIL_HOST_PASSWORD')
+
 
 
 
