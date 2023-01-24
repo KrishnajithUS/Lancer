d161db5e8e923bb73cb481727a1baf71825e89a2
krishnajith <krishnajithus673@gmail.com>
2023-01-24 21:40:02 +0000

    ns

--- backend/lancer/asgi.py
+++ backend/lancer/asgi.py
@@ -7,10 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lancer.settings")
+
+import django
+django.setup()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lancer.settings")
 django_asgi_app = get_asgi_application()
 from django_channels_jwt_auth_middleware.auth import JWTAuthMiddlewareStack
 
