f59a670fde1264961af749d771231381a1ed26eb
krishnajith <krishnajithus673@gmail.com>
2023-01-25 07:39:04 +0000

    https added

--- backend/lancer/urls.py
+++ backend/lancer/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path("", include("chat.urls")),
     path("api/", include("payment.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
