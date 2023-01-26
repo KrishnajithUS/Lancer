1d615ff39b8efc968dc36fb4ef140397eea3bc89
krishnajith <krishnajithus673@gmail.com>
2023-01-26 19:41:01 +0000

    t2

--- backend/lancer/asgi.py
+++ backend/lancer/asgi.py
@@ -15,6 +15,7 @@ django.setup()
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lancer.settings")
 django_asgi_app = get_asgi_application()
 from django_channels_jwt_auth_middleware.auth import JWTAuthMiddlewareStack
 
@@ -26,8 +27,10 @@ import chat.routing
 
 application = ProtocolTypeRouter(
     {
-        "http": django_asgi_app,
-        "websocket": AllowedHostsOriginValidator(
+   "websocket":JWTAuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns)),
+
+   "http":django_asgi_app,
+   "websocket": AllowedHostsOriginValidator(
             JWTAuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns)))
     }
 )
