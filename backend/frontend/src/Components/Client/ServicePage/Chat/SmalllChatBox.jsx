132d0a4d107183a2954ae31b7382502c8dcea355
krishnajith <krishnajithus673@gmail.com>
2023-01-27 03:20:24 +0530

    t6

--- backend/frontend/src/Components/Client/ServicePage/Chat/SmalllChatBox.jsx
+++ backend/frontend/src/Components/Client/ServicePage/Chat/SmalllChatBox.jsx
@@ -16,7 +16,7 @@ function SmalllChatBox({ modal, showModal, post }) {
   const namesAlph = [username, freelancerUsername].sort();
   const conversationName = `${namesAlph[0]}__${namesAlph[1]}`;
   const { sendJsonMessage } = useWebSocket(
-    authTokens ? `wss://lan-cer.online/${conversationName}/` : null,
+    authTokens ? `ws://lan-cer.online/${conversationName}/` : null,
     {
       queryParams: {
         token: authTokens,
